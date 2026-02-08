2e5ca9e2b8e7d1a2dd016c1417b64cc12157c6c8
MarcosSouzaa <marcossouza.git@gmail.com>
2026-02-08 11:02:35 -0300

    commit inicial

---
+++ inovasouzza-servicos/src/test/java/com/inovasouzza/servicos/ServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovasouzza.servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
