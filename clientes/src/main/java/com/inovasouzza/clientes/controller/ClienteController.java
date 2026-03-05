e9f1c756f0afafbff1c9f4638bfbfb8d43f4372e
MarcosSouzaa <marcossouza.git@gmail.com>
2026-03-05 11:17:18 -0300

    atualização do controller e service com implementação do CRUD

--- clientes/src/main/java/com/inovasouzza/clientes/controller/ClienteController.java
+++ clientes/src/main/java/com/inovasouzza/clientes/controller/ClienteController.java
@@ -4,9 +4,11 @@ import com.inovasouzza.clientes.model.Cliente;
 import com.inovasouzza.clientes.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +25,9 @@ public class ClienteController {
     // Method Cadastrar/Salvar
     @PostMapping
     public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente) {
-        service.salvar(cliente);
-        return ResponseEntity.ok(cliente);
+        // Salve e pegue o objeto atualizado que o banco devolve
+        Cliente clienteSalvo = service.salvar(cliente);
+        return ResponseEntity.ok(clienteSalvo);
     }
 
     // Method OBTER DADOS
@@ -33,8 +36,7 @@ public class ClienteController {
         return service
                 .obterPorCodigo(codigo)
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
+                .orElseGet(() -> ResponseEntity.notFound().build());    }
 
     // Method LISTAR TODOS
     @GetMapping
@@ -42,4 +44,24 @@ public class ClienteController {
         List<Cliente> clientes = service.listarTodos();
         return ResponseEntity.ok(clientes);
     }
+
+    // Method ATUALIZAR
+    @PutMapping("{codigo}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable("codigo") Long codigo, @RequestBody Cliente cliente) {
+        return service.obterPorCodigo(codigo).map(clienteExistente -> {
+            cliente.setCodigo(codigo); // Garante que o ID da URL seja o mesmo do objeto
+            service.atualizar(cliente);
+            return ResponseEntity.ok(cliente);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Method DELETAR
+    @DeleteMapping("{codigo}")
+    public ResponseEntity<Void> deletar(@PathVariable("codigo") Long codigo) {
+        return service.obterPorCodigo(codigo).map(cliente -> {
+            service.deletar(codigo);
+            return ResponseEntity.noContent().<Void>build(); // Retorna 204 No Content
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
+
--- clientes/src/main/java/com/inovasouzza/clientes/service/ClienteService.java
+++ clientes/src/main/java/com/inovasouzza/clientes/service/ClienteService.java
@@ -4,7 +4,7 @@ import com.inovasouzza.clientes.model.Cliente;
 import com.inovasouzza.clientes.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,16 +15,31 @@ public class ClienteService {
     private final ClienteRepository repository;
 
     //Method cadastrar/salvar cliente
+    @Transactional // Garante que o banco sincronize na hora
     public Cliente salvar(Cliente cliente ) {
         return repository.save(cliente);
     }
 
+    @Transactional(readOnly = true) // Leitura otimizada
     //Method buscar cliente por código
     public Optional<Cliente>obterPorCodigo(Long codigo) {
         return repository.findById(codigo);
     }
 
+    @Transactional(readOnly = true) // Leitura otimizada
     public List<Cliente> listarTodos() {
         return repository.findAll(); // O JpaRepository já fornece esse method pronto
+    }
+
+    // Method Atualizar
+    @Transactional // Escrita'
+    public Cliente atualizar(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    // Method Deletar
+    @Transactional // Escrita
+    public void deletar(Long codigo) {
+        repository.deleteById(codigo);
     }
 }
